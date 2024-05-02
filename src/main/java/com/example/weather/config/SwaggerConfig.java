9604912893bc98888af28094946ed01538df11f4
minzion <aert12435@gmail.com>
2024-05-02 10:02:39 +0900

    Swagger 설정 완료

--- src/main/java/com/example/weather/config/SwaggerConfig.java
+++ src/main/java/com/example/weather/config/SwaggerConfig.java
@@ -1,69 +1,26 @@
-//package com.example.weather.config;
-//
-//
-//import io.swagger.v3.oas.models.Components;
-//import io.swagger.v3.oas.models.OpenAPI;
-//import io.swagger.v3.oas.models.info.Info;
-//import org.springdoc.core.models.GroupedOpenApi;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//@Configuration
-//public class SwaggerConfig {
-//    @Bean
-//    public GroupedOpenApi SignApi() {
-//        return GroupedOpenApi.builder()
-//                .group("sign")
-//                .pathsToMatch("/api/sign-up","/api/sign-in","/api/refresh-token")
-//                .build();
-//    }
-//
-//    @Bean
-//    public GroupedOpenApi MemberApi() {
-//        return GroupedOpenApi.builder()
-//                .group("member")
-//                .pathsToMatch("/api/members/**")
-//                .build();
-//    }
-//
-//    @Bean
-//    public GroupedOpenApi CategoryApi() {
-//        return GroupedOpenApi.builder()
-//                .group("category")
-//                .pathsToMatch("/api/categories/**")
-//                .build();
-//    }
-//
-//    @Bean
-//    public GroupedOpenApi PostApi() {
-//        return GroupedOpenApi.builder()
-//                .group("post")
-//                .pathsToMatch("/api/posts/**")
-//                .build();
-//    }
-//
-//    @Bean
-//    public GroupedOpenApi CommentApi() {
-//        return GroupedOpenApi.builder()
-//                .group("comment")
-//                .pathsToMatch("/api/comments/**")
-//                .build();
-//    }
-//
-//    @Bean
-//    public GroupedOpenApi MessageApi() {
-//        return GroupedOpenApi.builder()
-//                .group("message")
-//                .pathsToMatch("/api/messages/**")
-//                .build();
-//    }
-//
-//    @Bean
-//    public OpenAPI customOpenAPI() {
-//        return new OpenAPI()
-//                .info(new Info()
-//                        .title("O2O Automatic Store System Demo")
-//                        .version("1.0")
-//                        .description("O2O Automatic Store System Demo REST API Documentation"))
-//                .components(new io.swagger.v3.oas.models.Components());
-//    }
-//}
+package com.example.weather.config;
+
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo(){
+        return new Info()
+                .title("날씨 일기 프로젝트 :)")
+                .version("1.1")
+                .description("날씨 일기를 CRUD 할 수 있는 프로젝트 입니다.");
+    }
+}
--- src/main/java/com/example/weather/controller/DiaryController.java
+++ src/main/java/com/example/weather/controller/DiaryController.java
@@ -2,7 +2,9 @@ package com.example.weather.controller;
 
 import com.example.weather.domain.Diary;
 import com.example.weather.service.DiaryService;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -17,34 +19,35 @@ import java.util.List;
 @RequiredArgsConstructor
 public class DiaryController {
     private final DiaryService diaryService;
-    @Operation(summary = "일기 생성")
+    @Operation(summary = "일기 텍스트와 날씨를 이용해서 DB에 일기 저장", description = "노드")
     @PostMapping("/create/diary")
     public void createDiary(
             @RequestParam
-            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date,
-            @RequestBody String text) {
+            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) @Parameter(description = "날짜형식 : yyyy-MM-dd",example = "2024-04-30") LocalDate date,
+            @RequestBody  String text) {
 
         diaryService.createDiary(date,text);
     }
-
+    @Operation(summary = "선택한 날짜의 모든 일기 데이터를 가져 옴니다")
     @GetMapping("/read/diary")
-    public List<Diary> readDiary(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date){
+    public List<Diary> readDiary(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE ) @Parameter(description = "날짜형식 : yyyy-MM-dd",example = "2024-04-30") LocalDate date){
       return diaryService.readDiary(date);
     }
-
+    @Operation(summary = "선택한 범위의 모든 일기 데이터를 가져 옵니다")
     @GetMapping("/read/diarys")
-    public List<Diary> readDiarys(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
-                                  @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDate){
+    public List<Diary> readDiarys(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) @Parameter(description = "검색 범위 시작일 : yyyy-MM-dd",example = "2024-04-30") LocalDate startDate,
+                                  @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) @Parameter(description = "검색 범위 종료일 : yyyy-MM-dd",example = "2024-04-30") LocalDate endDate){
         return diaryService.readDiarys(startDate,endDate);
     }
-
+    @Operation(summary = "선택한 날자의 일기를 텍스트를 이용해 수정합니다 수정합니다")
     @PutMapping("/update/diary")
-    void updateDiary(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date,
-                    @RequestBody String text){
+    void updateDiary(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) @Parameter(description = "날짜형식 : yyyy-MM-dd",example = "2024-04-30") LocalDate date,
+                    @RequestBody  String text){
         diaryService.updateDiary(date,text);
     }
+    @Operation(summary = "선택한 날짜의 일기를 제거합니다.")
     @DeleteMapping("/delete/diary")
-    void deleteDiary(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date){
+    void deleteDiary(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) @Parameter(description = "날짜형식 : yyyy-MM-dd",example = "2024-04-30") LocalDate date){
         diaryService.deleteDiary(date);
     }
 }
